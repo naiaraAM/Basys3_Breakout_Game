afad56bdab874ac26a872c44d0cf268c0e49a7e6
Pablo Goitia <pgg566@alumnos.unican.es>
2023-12-26 19:15:13 +0100

    Fixed smoke position, again :(

--- task_05/task_05.sdk/example/src/main.c
+++ task_05/task_05.sdk/example/src/main.c
@@ -125,7 +125,7 @@ int main(){
 void life_lost()
 {
 	position_t pos = {bola.x - SMOKE_WIDTH / 2, bola.y - SMOKE_HEIGHT};
-	paint_animation(pos, smoke, SMOKE_FRAMES, SMOKE_TIME, SMOKE_WIDTH, SMOKE_HEIGHT);
+	paint_animation(pos, smoke, SMOKE_FRAMES, SMOKE_TIME, SMOKE_HEIGHT, SMOKE_WIDTH);
 	init_ball();
 }
 
