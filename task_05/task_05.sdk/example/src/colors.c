a633953804004a09bd42d9a422c71a2478210ea5
naiaraAM <nam312@alumnos.unican.es>
2023-12-30 13:19:08 +0100

    beautified colors

--- task_05/task_05.sdk/example/src/colors.c
+++ task_05/task_05.sdk/example/src/colors.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "colors.h"
 
-bool equals_color(color_t color1, color_t color2)
-{
+bool equals_color(color_t color1, color_t color2) {
 	return ((color1.r == color2.r) && (color1.g == color2.g) && (color1.b == color2.b));
 }
